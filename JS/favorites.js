e83b2710395fdba5571ed6294429669bc81ee16d
Daniela <trindadedv@gmail.com>
2023-03-21 19:29:28 +0100

    class GithubUser in another file and imported into favorites.js

--- JS/favorites.js
+++ JS/favorites.js
@@ -1,16 +1,4 @@
-export class GithubUser {
-    static search(username) {
-        const endpoint = `https://api.github.com/users/${username}`
-        return fetch(endpoint)
-        .then(data => data.json())
-        .then(data => ({
-            login: data.login,
-            name: data.name,
-            public_repos: data.public_repos,
-            followers: data.followers
-        }))
-    }
-}
+import { GithubUser } from "./githubUser.js"
 
 export class Favorites {
     constructor(root) {
